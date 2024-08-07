56596bfee60a0046fde9847a49bc4491aab8ad27
ursapictura <hsmithfreelance@gmail.com>
2024-08-06 19:59:08 -0500

    Fixed symbol errors in post contents

--- Program.cs
+++ Program.cs
@@ -125,7 +125,7 @@ List<Post> posts = new()
         CategoryId = 4,
         Title = "Moby-Dick",
         PublicationDate = new DateTime(1851, 11, 21),
-        Content = "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world."
+        Content = "Call me Ishmael. Some years ago, never mind how long precisely, having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world."
     },    
     new() 
     {
