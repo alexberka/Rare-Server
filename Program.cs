b55a7751088743d8001d8500ca7cd19c278fc7bf
Jesse <frejes32@gmail.com>
2024-08-09 18:14:30 -0500

    Removed unused localhost

--- Program.cs
+++ Program.cs
@@ -223,7 +223,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5003")
+        policy.WithOrigins("http://localhost:3000")
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader();
